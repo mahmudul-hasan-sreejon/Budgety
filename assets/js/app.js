5cc0cfdbb42eeaed567e54fc738e6f2a8e1fc090
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-02-04 17:12:36 +0600

    event listener added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 
 // Budget Controller
 let budgetController = (function() {
-    
 })();
 
 
@@ -12,5 +11,18 @@ let UIController = (function() {
 
 // Global App Controller
 let controller = (function(budgetCtrl, UICtrl) {
-    
+
+    let ctrtAddItem = function() {
+        console.log('OK');
+    };
+
+
+    document.addEventListener('keypress', function(e) {
+        if(e.keyCode === 13 || e.which === 13) {
+            ctrtAddItem();
+        }
+    });
+
+    document.querySelector('.add__btn').addEventListener('click', ctrtAddItem);
+
 })(budgetController, UIController);
