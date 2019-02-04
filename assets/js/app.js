2447300bd4daf157679508be0c69345ce6001b0e
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-02-04 16:52:04 +0600

    base controllers added

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+
+// Budget Controller
+let budgetController = (function() {
+    
+})();
+
+
+// UI Controller
+let UIController = (function() {
+})();
+
+
+// Global App Controller
+let controller = (function(budgetCtrl, UICtrl) {
+    
+})(budgetController, UIController);
