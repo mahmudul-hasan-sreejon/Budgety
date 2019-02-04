956614f5c03133f862b43424012b7d471191ec33
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-02-04 19:18:58 +0600

    DOMstrings method added

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,23 +6,58 @@ let budgetController = (function() {
 
 // UI Controller
 let UIController = (function() {
+
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+            
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+
 })();
 
 
 // Global App Controller
 let controller = (function(budgetCtrl, UICtrl) {
 
+    let setupEventListeners = function() {
+        let DOM = UICtrl.getDOMstrings();
+
+        document.addEventListener('keypress', function(e) {
+            if(e.keyCode === 13 || e.which === 13) {
+                ctrtAddItem();
+            }
+        });
+    
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrtAddItem);
+    }
+
     let ctrtAddItem = function() {
-        console.log('OK');
+        let inputs = UICtrl.getInput();
     };
 
-
-    document.addEventListener('keypress', function(e) {
-        if(e.keyCode === 13 || e.which === 13) {
-            ctrtAddItem();
+    return {
+        init: function() {
+            console.log('Application has started...');
+            setupEventListeners();
         }
-    });
-
-    document.querySelector('.add__btn').addEventListener('click', ctrtAddItem);
+    };
 
 })(budgetController, UIController);
+
+controller.init();
